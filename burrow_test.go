4ad3ddc48f638e1431e32cd69e0c16a405dfb400
wfnuser <wfnuser@126.com>
2020-03-23 00:47:01 +0800

    test: add tricky test case

--- burrow_test.go
+++ burrow_test.go
@@ -16,3 +16,22 @@ func TestGetBurrow(t *testing.T) {
 	nb := GetBurrow("test")
 	fmt.Printf("namespace is: %s \n", nb.namespace)
 }
+
+func increment(b *Burrow, key string) {
+	v, ok := b.Get(key)
+
+	if ok {
+		b.Put(key, v.(int)+1)
+	}
+}
+
+func TestPutBurrow(t *testing.T) {
+	b := NewBurrow("test", 2)
+	b.Put("test", 0)
+	for i := 0; i < 100; i++ {
+		go increment(b, "test")
+	}
+	v, ok := b.Get("test")
+
+	fmt.Printf("%v %v", v, ok)
+}
